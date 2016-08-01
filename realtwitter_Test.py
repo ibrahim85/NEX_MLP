8f695f4b8a0c1aee7328270e5b76836c936a0c8c
newera912 <alex_912@live.cn>
2016-08-01 15:17:35 -0400

    aalim

--- realtwitter_Test.py
+++ realtwitter_Test.py
@@ -5,9 +5,14 @@ import json
 
 with gzip.open("/home/kitware/aalim/data/Twitter/tweets.2013-05-01T00%3A00.M=000.gz","rb") as tf:
     for i,line in enumerate(tf.readlines()):
-        #print line
+        print"---------------------------------\n"
+        print line
+
         if i<10:
             twitter=json.loads(line)
-            print twitter.keys()
+            print(json.dumps(twitter, indent=4))
+            keys_list=twitter.keys()
+            #for key,value in twitter.items():
+            #    print key,value
         else:
             break
