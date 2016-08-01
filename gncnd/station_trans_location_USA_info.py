5bb0efdf64a2988ecf482a8b1f2a2ee27c5d496c
newera912 <alex_912@live.cn>
2016-08-01 13:17:58 -0400

    aalim

--- gncnd/station_trans_location_USA_info.py
+++ gncnd/station_trans_location_USA_info.py
@@ -67,7 +67,7 @@ def main():
 
     with open("USA_ghcnd-stations-loc.txt","r") as gsT:
         for i,line in enumerate(gsT.readlines()):
-            if i>8543:
+            if i>12163:
                 terms=line.split()
 
                 geo_code=str(terms[1])+","+str(terms[2])
--- realtwitter_Test.py
+++ realtwitter_Test.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import gzip
+import json
 
 
-with open("~/home/kitware/aalim/data/")
+with gzip.open("/home/kitware/aalim/data/Twitter/tweets.2013-05-01T00%3A00.M=000.gz","rb") as tf:
+    for i,line in enumerate(tf.readlines()):
+        #print line
+        if i<10:
+            twitter=json.loads(line)
+            print twitter.keys()
+        else:
+            break
